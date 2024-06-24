eda1a6397a99a2eff3683fcd712bcbddb3b5f5e4
jsb5324 <134386602+jsb5324@users.noreply.github.com>
2024-06-24 14:16:29 +0900

    Add files via upload

--- ww/app.js
+++ ww/app.js
@@ -12,3 +12,38 @@ const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelectorAll("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
+
+//생략
+let playerLoc = stageSize*(stageSize-2)+7 //202;
+
+function makePlayer() {
+    stage[playerLoc].classList.add("player");
+ }
+ makePlayer()
+
+function movePlayer(e) { }
+
+document.addEventListener("keyup", movePlayer)
+
+function movePlayer(e) {
+    stage[playerLoc].classList.remove("player");
+    switch (e.keycode) {
+        case 37: //왼쪽
+            playerLoc--;
+            break;
+        case 39: //오른쪽
+            playerLoc++;
+            break;    
+    }
+    stage[player].classList.add("player");
+}
+
+//생략
+let invaderLoc;
+let gameInterval;
+
+function makeInvader() { }
+
+function moveInvader() { }
+
+interval = setInterval(moveInvader, 1000);
