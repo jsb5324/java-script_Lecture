376d93fe24e347f3ba3802fc34e3569464eb22ca
jsb5324 <134386602+jsb5324@users.noreply.github.com>
2024-06-24 13:22:33 +0900

    Add files via upload

---
+++ ww/app.js
@@ -0,0 +1,14 @@
+const stageSize = 15;
+
+//스테이지 생성
+for(let i=0; i < stageSize*stageSize; i++) {
+    const box = document.createElement("div");
+    document.querySelector(".grid").appendChild(box);
+
+}
+
+const grid = document.querySelector(".grid");
+const stage = document.querySelectorAll(".grid div");
+const startBtn = document.querySelectorAll("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const display = document.querySelector("#display");
